1ce7da6ac4168feac7276f6c2b8166a3acaa94a9
LaoSparrow <laosparrow@outlook.com>
2024-06-15 15:22:08 +0800

    fix: transform_modification jittering

--- example/src/scripts/transform_modification.cpp
+++ example/src/scripts/transform_modification.cpp
@@ -6,6 +6,13 @@
 
 using namespace SparrowEngine::Example::Scripts;
 
+void TransformModification::start() {
+    auto parent_obj = game_object.lock();
+    orig_quat = parent_obj->transform.rotation;
+    euler_angle = glm::degrees(glm::eulerAngles(parent_obj->transform.rotation));
+    Behavior::start();
+}
+
 void TransformModification::update() {
     auto parent_obj = game_object.lock();
     if (ImGui::Begin((parent_obj->name + " transform").c_str())) {
@@ -13,10 +20,17 @@ void TransformModification::update() {
         ImGui::SliderFloat3("scale", (float *)&parent_obj->transform.scale, 0.0f, 5.0f);
         ImGui::SliderFloat4("rotation", (float *)&parent_obj->transform.rotation, 0.0f, 1.0f);
 
-        glm::vec3 euler_angle = glm::degrees(glm::eulerAngles(parent_obj->transform.rotation));
-        if (ImGui::SliderFloat3("euler angle", glm::value_ptr(euler_angle), -360.0f, 360.0f))
+        if (orig_quat != parent_obj->transform.rotation) {
+            orig_quat = parent_obj->transform.rotation;
+            euler_angle = glm::degrees(glm::eulerAngles(parent_obj->transform.rotation));
+        }
+
+        if (ImGui::SliderFloat3("euler angle", glm::value_ptr(euler_angle), -360.0f, 360.0f)) {
             parent_obj->transform.rotation = glm::quat(glm::radians(euler_angle));
+            orig_quat = parent_obj->transform.rotation;
+        }
 
         ImGui::End();
     }
+    Behavior::update();
 }
--- example/src/scripts/transform_modification.hpp
+++ example/src/scripts/transform_modification.hpp
@@ -2,11 +2,19 @@
 
 #include "sparrow_engine/behavior.hpp"
 
+#include "glm/gtc/quaternion.hpp"
+#include "glm/vec3.hpp"
+
 namespace SparrowEngine::Example::Scripts {
     class TransformModification : public SparrowEngine::Behavior {
+    private:
+        glm::vec3 euler_angle;
+        glm::quat orig_quat;
+
     public:
         using SparrowEngine::Behavior::Behavior;
 
+        void start() override;
         void update() override;
     };
 }
