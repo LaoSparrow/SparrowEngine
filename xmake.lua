e9c79ac5cbe56e375b460089a09eb13a26b53a11
LaoSparrow <laosparrow@outlook.com>
2024-08-16 19:29:45 +0800

    chore: update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,13 +1,20 @@
 add_rules("mode.debug", "mode.release")
 
+set_encodings("utf-8")
+if is_mode("release") then
+    set_runtimes("MT")
+else
+    set_runtimes("MTd")
+end
+
 add_requires(
-        "vcpkg::glad",
-        "vcpkg::glfw3",
-        "vcpkg::glm",
-        "vcpkg::Stb",
-        "vcpkg::imgui",
-        "vcpkg::assimp",
-        "vcpkg::fmt", {debug = is_mode("debug")})
+        "vcpkg::glad >=0.1.36",
+        "vcpkg::glfw3 >=3.4",
+        "vcpkg::glm >=1.0.1",
+        "vcpkg::Stb 2023-04-11",
+        "vcpkg::imgui >=1.90.6",
+        "vcpkg::assimp >=5.4.0",
+        "vcpkg::fmt >=10.2.1", {debug = is_mode("debug")})
 
 target("SparrowEngine")
     set_languages("c++20")
@@ -25,6 +32,9 @@ target("SparrowEngine")
             "vcpkg::imgui",
             "vcpkg::assimp",
             "vcpkg::fmt", {public = true})
+    if is_plat("windows") then
+        add_syslinks("shell32")
+    end
 
 target("SparrowEngine.Example")
     set_languages("c++20")
@@ -32,7 +42,7 @@ target("SparrowEngine.Example")
     add_files("example/src/**.cpp")
     add_includedirs("example/src/")
     add_deps("SparrowEngine")
-    --after_build(function (target)
-    --    os.rm("$(buildir)/$(plat)/$(arch)/$(mode)/resources")
-    --    os.cp("$(projectdir)/example/resources/", "$(buildir)/$(plat)/$(arch)/$(mode)/")
-    --end)
+    after_build(function (target)
+        os.rm(target:targetdir() .. "/resources")
+        os.cp("$(projectdir)/example/resources/", target:targetdir())
+    end)
